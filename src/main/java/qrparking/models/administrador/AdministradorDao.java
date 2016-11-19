d850bf66feee9874ccaa95ab6f9e5737f09d6b5c
hugodesof <hugodesof@gmail.com>
2016-11-19 17:18:44 -0200

    Criptografia da senha e Validação de CPF existente

--- src/main/java/qrparking/models/administrador/AdministradorDao.java
+++ src/main/java/qrparking/models/administrador/AdministradorDao.java
@@ -9,49 +9,57 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 /**
- * This class is used to access data for the User entity.
- * Repository annotation allows the component scanning support to find and 
- * configure the DAO wihtout any XML configuration and also provide the Spring 
- * exceptiom translation.
+ * This class is used to access data for the User entity. Repository annotation
+ * allows the component scanning support to find and configure the DAO wihtout
+ * any XML configuration and also provide the Spring exceptiom translation.
  * Since we've setup setPackagesToScan and transaction manager on
  * DatabaseConfig, any bean method annotated with Transactional will cause
- * Spring to magically call begin() and commit() at the start/end of the
- * method. If exception occurs it will also call rollback().
+ * Spring to magically call begin() and commit() at the start/end of the method.
+ * If exception occurs it will also call rollback().
  */
 @Repository
 @Transactional
 public class AdministradorDao {
-  
-  // ------------------------
-  // PUBLIC METHODS
-  // ------------------------
-  
-  public void create(Administrador administrador) {
-    entityManager.persist(administrador);
-    return;
-  }
-  
-  @SuppressWarnings("unchecked")
-  public List<Administrador> getAll() {
-    return entityManager.createQuery("from Administrador").getResultList();
-  }
 
-  public Administrador getById(long id) {
-    return entityManager.find(Administrador.class, id);
-  }
+	// ------------------------
+	// PUBLIC METHODS
+	// ------------------------
 
-  public void update(Administrador administrador) {
-    entityManager.merge(administrador);
-    return;
-  }
+	public void create(Administrador administrador) {
+		entityManager.persist(administrador);
+		return;
+	}
 
-  // ------------------------
-  // PRIVATE FIELDS
-  // ------------------------
-  
-  // An EntityManager will be automatically injected from entityManagerFactory
-  // setup on DatabaseConfig class.
-  @PersistenceContext
-  private EntityManager entityManager;
-  
+	@SuppressWarnings("unchecked")
+	public List<Administrador> getAll() {
+		return entityManager.createQuery("from Administrador").getResultList();
+	}
+
+	public Administrador getById(long id) {
+		return entityManager.find(Administrador.class, id);
+	}
+
+	public boolean getCpfExistente(String cpf, long id) {
+		if (id == 0) {
+			return entityManager.createQuery("from Administrador where cpf = :cpf").setParameter("cpf", cpf)
+					.getResultList().size() > 0;
+		}
+		return entityManager.createQuery("from Administrador where cpf = :cpf and id != :id").setParameter("cpf", cpf)
+				.setParameter("id", id).getResultList().size() > 0;
+	}
+
+	public void update(Administrador administrador) {
+		entityManager.merge(administrador);
+		return;
+	}
+
+	// ------------------------
+	// PRIVATE FIELDS
+	// ------------------------
+
+	// An EntityManager will be automatically injected from entityManagerFactory
+	// setup on DatabaseConfig class.
+	@PersistenceContext
+	private EntityManager entityManager;
+
 } // class UserDao
--- src/main/java/qrparking/service/administrador/AdministradorService.java
+++ src/main/java/qrparking/service/administrador/AdministradorService.java
@@ -1,5 +1,8 @@
 package qrparking.service.administrador;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,7 +25,42 @@ public class AdministradorService {
 		if (administrador.getId() == 0) {
 			administrador.setDtCadastro(new Date());
 		}
+
+		if (administradorDao.getCpfExistente(administrador.getCpf(), administrador.getId())) {
+			throw new IllegalArgumentException("CPF já cadastrado");
+		}
+
+		String senhaCripto = criptografarSenha(administrador);
+
+		administrador.setSenha(senhaCripto);
+
 		administradorDao.update(administrador);
+
+	}
+
+	private String criptografarSenha(Administrador administrador) {
+		String senhaOriginal = administrador.getSenha();
+
+		MessageDigest algorithm;
+		byte[] messageDigest = null;
+		try {
+			algorithm = MessageDigest.getInstance("SHA-256");
+			try {
+				messageDigest = algorithm.digest(senhaOriginal.getBytes("UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		StringBuilder hexString = new StringBuilder();
+		for (byte b : messageDigest) {
+			hexString.append(String.format("%02X", 0xFF & b));
+		}
+
+		String senhaCripto = hexString.toString();
+		return senhaCripto;
 	}
 
 	public List<Administrador> buscarTodos() {
@@ -30,7 +68,7 @@ public class AdministradorService {
 		administrador = administradorDao.getAll();
 		return administrador;
 	}
-	
+
 	public Administrador buscarPorId(@PathVariable("administradorId") Long administradorId) {
 		Administrador administrador = administradorDao.getById(administradorId);
 		return administrador;
